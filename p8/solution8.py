dc4f8ed4c92247ee6c93ecd294cbc4af362a0884
JamesMcGaa <jemcgaa@gmail.com>
2021-12-08 23:36:38 -0500

    p8 attempt 2

--- p8/solution8.py
+++ p8/solution8.py
@@ -15,78 +15,97 @@ for line in input_cast:
 print(counter)
 
 ALPHABET = 'abcdefg'
+BIG_NUMS_TO_TRUES = {
+    0:set('abcefg'),
+    1:set('cf'),
+    2:set('acdeg'),
+    3:set('acdfg'),
+    4:set('bcdf'),
+    5:set('abdfg'),
+    6:set('abdefg'),
+    7:set('acf'),
+    8:set('acbdefg'),
+    9:set('abcdfg'),
+}
+SEQ_LENGTH_TO_BIG_NUMS = {
+    2:[1],
+    3:[7],
+    4:[4],
+    5:[2,3,5],
+    6:[6,9,0],
+    7:[8],
+}
+
+def sanity_test_big_number_perm(big_number_perm, pre):
+    for seq_ind, big_number in enumerate(big_number_perm):
+        seq = pre[seq_ind]
+        if not big_number in SEQ_LENGTH_TO_BIG_NUMS[len(seq)]:
+            return False
+    return True
+
+def alphabet_perm_works(alphabet_perm, raw_to_potential_true):
+    for raw_ind, true in enumerate(alphabet_perm):
+        raw = ALPHABET[raw_ind]
+        if not true in raw_to_potential_true[ALPHABET[raw_ind]]:
+            return False
+
+    return True
+
 # part b
 def solve(pre, post):
-    raw_to_true_mapping = {letter:set(ALPHABET) for letter in ALPHABET}
+    # seq_length_to_seqs = {length:[] for length in range(2,8)} 
 
-    for seq in pre: #1
-        if len(seq) == 2:
-            for letter in seq:
-                raw_to_true_mapping[letter] = raw_to_true_mapping[letter] & set('cf')
-
-    for seq in pre: #7s
-        if len(seq) == 3:
-            for letter in seq:
-                raw_to_true_mapping[letter] = raw_to_true_mapping[letter] & set('acf')
-
-    for seq in pre: #4s
-        if len(seq) == 4:
-            for letter in seq:
-                raw_to_true_mapping[letter] = raw_to_true_mapping[letter] & set('bcdf')
-
-    for seq in pre: #2,3,5
-        if len(seq) == 5:
-            for letter in seq:
-                raw_to_true_mapping[letter] = raw_to_true_mapping[letter] & set('bcdf')
-
-    for seq in pre: #0,6,9
-        if len(seq) == 6:
-            for letter in seq:
-                raw_to_true_mapping[letter] = raw_to_true_mapping[letter] & set('bcdf')
+    # for seq in pre: #1
+    #     seq_length_to_seqs[len(seq)].append(seq)
     
-    # nothing learned from 8s 
-    
-    mapping = {}
-    perms = [''.join(p) for p in permutations(ALPHABET)]
-    for perf2 in perms:
-        for perm in perms:
-            good = True
-            for i in range(len(ALPHABET)):
-                true = ALPHABET[i]
-                proposed = perm[i]
-                if not true in raw_to_true_mapping[proposed]:
-                    good = False
-                    break
+
+    big_number_perms = list(permutations([index for index in range(10)])) 
+    for big_number_perm in big_number_perms:
+        if sanity_test_big_number_perm(big_number_perm, pre):
+
+            raw_to_potential_true = {}
+            for seq_index, big_number in enumerate(big_number_perm):
+                seq = pre[seq_index]
+                for letter in seq: 
+                    raw_to_potential_true[letter] = BIG_NUMS_TO_TRUES[big_number]
             
-            if good:
-                for i in range(len(ALPHABET)):
-                    mapping[ALPHABET[i]] = perm[i]
-                break
-    print(mapping)
+            alphabet_perms = list(permutations([ALPHABET[index] for index in range(len(ALPHABET))]))
+            for alphabet_perm in alphabet_perms:
+                if alphabet_perm_works(alphabet_perm, raw_to_potential_true):
+
+                    for pre_seq in pre:
+                        for letter in pre_seq:
+                            if letter not in 
+                    for raw_ind, true in enumerate(alphabet_perm):
+                        raw = ALPHABET[raw_ind]
+
+
+                    for post_seq in post:
+                        pre_ind = big_number_perm[pre.index(post_seq)]
+                        print(pre_ind)
+                    
+                    # return big_number_perm
+
+
+
         
-    # true_to_raw_mapping = {letter:set() for letter in 'abcdefg'}
-    # for true in 'abcdefg':
-    #     for raw, possible_trues in raw_to_true_mapping.items():
-    #         if true in possible_trues:
-    #             true_to_raw_mapping[true].add(raw)
 
 
         
     
 
-    
-
-    
-    for line in raw_to_true_mapping:
-        print(line, raw_to_true_mapping[line])
-
-    # for line in true_to_raw_mapping:
-    #     print(line, true_to_raw_mapping[line])
-
-def solved(mapping):
-    return all([len(mapping[key]) == 1 for key in mapping])
+# def solved(mapping):
+#     return all([len(mapping[key]) == 1 for key in mapping])
 
 
 for line in input_cast[:1]:
     pre, post = line
-    solve(pre, post)
+    for ind, seq in enumerate(pre):
+        pre[ind] = sorted(seq)
+    for ind, seq in enumerate(post):
+        post[ind] = sorted(seq)
+
+    perm = solve(pre, post)
+    # for post_seq in post:
+    #     pre_ind = perm[pre.index(post_seq)]
+    #     print(pre_ind)
