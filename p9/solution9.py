41169195f6c38ec7260595440f81981e297bd4a2
JamesMcGaa <jemcgaa@gmail.com>
2021-12-10 00:11:34 -0500

    p9

--- p9/solution9.py
+++ p9/solution9.py
@@ -1,4 +1,6 @@
 import os 
+import numpy as np
+
 f = open("input9.txt", "r")
 grid = [[int(digit) for digit in line.strip()] for line in  f.readlines()]
 
@@ -24,3 +26,40 @@ for x in range(len(grid)):
 
 print(counter)
 
+
+#b
+def bfs(x, y, grid, connected_component_sizes, seen):
+    stack = [(x,y)]
+    pre_size = len(seen)
+    while len(stack) > 0:
+        current = stack.pop()
+        i,j = current
+        seen.add(current)
+        for x_offset in [-1, 1]:
+            if (i + x_offset, j) not in seen and \
+                0 <= i + x_offset and i + x_offset < len(grid) and \
+                grid[i + x_offset][j] >= grid[i][j] and \
+                grid[i + x_offset][j] != 9:
+
+                stack.append((i + x_offset, j))
+
+        for y_offset in [-1, 1]:
+            if (i, j + y_offset) not in seen and \
+                0 <= j + y_offset and j + y_offset < len(grid[i]) and \
+                grid[i][j + y_offset] >= grid[i][j]  and \
+                grid[i][j + y_offset] != 9:
+
+                stack.append((i, j + y_offset))
+
+    connected_component_sizes.append(len(seen) - pre_size)
+    
+
+
+connected_component_sizes = []
+seen = set()
+for x in range(len(grid)):
+    for y in range(len(grid[0])):
+        if not (x,y) in seen and is_low(x, y, grid):
+            bfs(x, y, grid, connected_component_sizes, seen)
+
+print(np.prod(list(sorted(connected_component_sizes, reverse=True))[:3]))
